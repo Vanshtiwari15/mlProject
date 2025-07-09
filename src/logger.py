091332d4c53bc5022b2b4eb47698168c53b52df6
Vansh Tiwari <vanshtiwari1512@gmail.com>
2025-07-09 21:10:18 +0530

    EDA and problem statement

--- src/logger.py
+++ src/logger.py
@@ -2,16 +2,33 @@ import logging
 import os
 from datetime import datetime
 
+# Create a unique log file name with timestamp
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+
+# Create a path to the logs directory
+logs_path=os.path.join(os.getcwd(),"logs")
 os.makedirs(logs_path,exist_ok=True)
 
+# Create full path to the log file
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
+# Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-
-
 )
+
+
+'''
+Think of a log as a diary entry for your program.
+
+    Whenever something important happens — like:
+
+        a function starts
+
+        a value is calculated
+
+        an error occurs
+
+'''
